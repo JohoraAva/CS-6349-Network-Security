44770b44f8ede29aaaf96bea6ba8c3c101ec0564
mahabhu <mashroor184@gmail.com>
2025-12-02 07:43:01 -0600

    done with library funcs

--- client.py
+++ client.py
@@ -1,10 +1,16 @@
 from utils import *
 
+
 # Global variables  
-pub_key = None
-pri_key = None
+session_keys = {}
+req_pri = {}
+p = {}
+g = {}
+lock = threading.Lock()
+
+
 def send_registration(s: socket.socket, id: str, private_key):
-    flag = b"00"
+    flag = b"0"
     id_bytes = id.encode()
     reg_msg = b"Registration info for " + id_bytes
     signed_msg = sign_message(private_key, reg_msg)
@@ -38,46 +44,32 @@ def receive(s,private_key):
             flag,src_id_b,dst_id_b, msg = data.split(b"|", 3)
             src_id = src_id_b.decode()
             dst_id = dst_id_b.decode()
-            if flag == b"01":
+            if flag == b"1":
 
-                # p, g, enc = msg.split(b"|",2)
                 print(f"[{src_id}] Session establishment request received.")
-                src_id, dst_id, p, g, ts_req, other_pub_key = handle_session_establish_request(msg, src_id, dst_id, private_key)
-                print("src_id : " + str(src_id) + ", dst_id : " + str(dst_id) + ", p : " + str(p) + ", g : " + str(g) + ", ts_req : " + str(ts_req) + ", other_pub_key : " + str(other_pub_key))
-                # print("# eikhane ashche")
-                # calculate session params
-                global pri_key
-                print("global val(): ", pri_key)
-                print("value: ", other_pub_key, get_own_pri_key(), p)
-                # shared_key = calculate_session_key(other_pub_key, get_own_pri_key(), p)
-                # session_establish_response(s,dst_id,src_id,private_key)
-
-                print(f"[{dst_id}] Session established with {src_id}. Shared session key: {shared_key}")
-            elif flag == b"10":
-                p, g, enc = msg.split(b"|",2)
+                p_, g_, ts_req, req_pub = handle_session_establish_request(msg, src_id, dst_id, private_key)
+                with lock:
+                    p[src_id] = p_
+                    g[src_id] = g_
+                    res_pub, res_pri = generate_dh_keypair(p[src_id], g[src_id])
+                    session_keys[src_id] = compute_shared_key(req_pub,res_pri,p[src_id])
+                session_establish_response(s,dst_id,src_id,private_key,res_pub)
+                with lock:
+                    print(f"[{dst_id}] Session established with {src_id}. Shared session key: {session_keys[src_id]}")
+            elif flag == b"2":
                 print(f"[{src_id}] Session establishment response received.")
-                # verify session params
-                # calculate session params
-                shared_key = calculate_session_key(other_pub_key, get_own_pri_key(), p)
-                print(f"[{dst_id}] Session established with {src_id}. Shared session key: {shared_key}")
-            elif flag == b"11":
+                ts_res, res_pub = handle_session_establish_response(msg, src_id, dst_id, private_key)
+                with lock:
+                    session_keys[src_id] = compute_shared_key(res_pub,req_pri[src_id],p[src_id])
+                    print(f"[{dst_id}] Session established with {src_id}. Shared session key: {session_keys[src_id]}")
+            elif flag == b"3":
+                with lock:
+                    msg = decrypt_with_shared_key(session_keys[src_id],msg)
                 print(f"\n[{src_id}]: {msg.decode()}\nid > ", end="")
             else:
                 print(f"[{src_id}] Invalid flag")
         except:
             break
-
-
-#global var, own_pri_key
-
-def set_own_pri_key(pri):
-    global pri_key
-    print("Setting own pri key: ", pri)
-    pri_key = pri
-
-def get_own_pri_key():
-    global pri_key
-    return pri_key
 
 
 def client():
@@ -97,12 +89,21 @@ def client():
         else:
             msg = input("Message: ").strip()
         if msg == "init":
-            global pub_key, pri_key
-            pub_key,  pri_key = session_establish_request(s, id, dst_id, private_key)
-            set_own_pri_key(pri_key)
-            print("init : "+str(pub_key)+", "+str(pri_key))
+            # global pub_key, pri_key
+            req_pub,  req_pri_, p_, g_ = session_establish_request(s, id, dst_id, private_key)
+            with lock:
+                req_pri[dst_id] = req_pri_
+                p[dst_id] = p_
+                g[dst_id] = g_
         else:
-            s.sendall(f"11|{id}|{dst_id}|{msg}".encode())
+            with lock:
+                sk = session_keys.get(dst_id)
+            if sk is None:
+                print(f"[!] Please establish session keys with {dst_id} first")
+            else:
+                with lock:
+                    cipher = encrypt_with_shared_key(session_keys[dst_id],msg.encode())
+                s.sendall(f"3|{id}|{dst_id}|".encode()+cipher)
         if dst_id.lower() == "exit":
             break
     s.close()
--- server.py
+++ server.py
@@ -29,7 +29,7 @@ def handle_client(c, addr):
             if not data:
                 return
             flag, rest = data.split(b"|", 1)
-            if flag == b"00":
+            if flag == b"0":
                 id, reg_msg, signed_msg = rest.split(b"|", 2)
                 id_str = id.decode()
                 print(f"[Relay] Registration request from {id_str}")
@@ -74,7 +74,6 @@ def all_client(s):
 
 
 def relay():
-    p, g = get_dh_params()
     s = socket.socket()
     s.bind((HOST, PORT))
     s.listen(50)
